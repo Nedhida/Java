8e8f29b56ed37b01a5026965507a71132f40290d
Nedhida <pinedoutshadeyouth@gmail.com>
2022-03-27 16:26:33 +0900

    Add fizzbuzz sample

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,9 @@ public class HelloWorld {
   // ↓ mainメソッドを定義する定型文
   public static void main(String[] args) {
     // ↓ JavaAPIであらかじめ用意、指定された文字列("")を標準出力に出力する一文
+    System.out.print("Hello World !!");
+    // println()は文字を表示した後に改行する
+    System.out.println("Hello World !!");
     System.out.println("Hello World !!");
   }
 }
