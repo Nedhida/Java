ff4bc9263ba62b748ef874695c46d11181c6f2d8
Nedhida <pinedoutshadeyouth@gmail.com>
2022-03-27 15:55:41 +0900

    Add how to java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// ↓ ファイル名はファイル内に宣言されたクラス名と一致している必要がある
+public class HelloWorld {
+  // ↓ mainメソッドを定義する定型文
+  public static void main(String[] args) {
+    // ↓ JavaAPIであらかじめ用意、指定された文字列("")を標準出力に出力する一文
+    System.out.println("Hello World !!");
+  }
+}
