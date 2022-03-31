65fc737ba2fa5f35d74b8f4b4a236644d519978e
Nedhida <pinedoutshadeyouth@gmail.com>
2022-03-31 12:01:03 +0900

    Add classes FizzBuzz2 can't use

--- Reverse.java
+++ Reverse.java
@@ -1,7 +1,12 @@
+// コマンドライン引数を逆向きに表示させる
+
 public class Reverse {
   public static void main(String[] args) {
+    // 引数の配列を後ろからループ
     for (int i = args.length - 1; i >= 0; i--) {
+      // 各引数の文字を後ろからループ
       for (int j = args[i].length() - 1; j >= 0; j--) {
+        // i番目の引数のj番目の文字を表示
         System.out.print(args[i].charAt(j));
       }
       System.out.print(" ");
@@ -10,5 +15,5 @@ public class Reverse {
   }
 }
 
-// エラー: メイン・クラスReverseを検出およびロードできませんでした
-// 原因: java.lang.ClassNotFoundException: Reverse
+// java Reverse this is a test とターミナルに入力すると
+// tset a si siht と出力される
